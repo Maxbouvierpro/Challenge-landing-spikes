57b3b0c02c136a0020df8fdf2e5cefe9ced8ac47
Maxbouvierpro <maxbouvier.pro@gmail.com>
2025-04-10 18:43:51 +0800

    landing page: Initial commit

---
+++ app/components/HeaderComponent.vue
@@ -0,0 +1,66 @@
+<script setup lang="ts">
+import type { NavigationMenuItem } from '@nuxt/ui'
+
+const items = ref<NavigationMenuItem[]>([
+  {
+    label: 'Features',
+    to: '/features',
+    children: [
+      {
+        label: 'Introduction',
+        description: 'Fully styled and customizable components for Nuxt.',
+      },
+      {
+        label: 'Installation',
+        description: 'Learn how to install and configure Nuxt UI in your application.',
+      },
+      {
+        label: 'Icons',
+        description: 'You have nothing to do, @nuxt/icon will handle it automatically.'
+      }
+    ]
+  },
+  {
+    label: 'Resources',
+    to: '/composables',
+    children: [
+      {
+        label: 'defineShortcuts',
+        description: 'Define shortcuts for your application.',
+        to: '/composables/define-shortcuts'
+      },
+      {
+        label: 'useOverlay',
+        description: 'Display a modal/slideover within your application.',
+        to: '/composables/use-overlay'
+      },
+      {
+        label: 'useToast',
+        description: 'Display a toast within your application.',
+        to: '/composables/use-toast'
+      }
+    ]
+  },
+  {
+    label: 'Contact Sales',
+    to: '/contact'
+  }
+])
+</script>
+
+<template>
+  <header class="flex items-center justify-between w-full">
+    <div class="flex items-center">
+      <NuxtImg 
+      src="/Frame.svg" />
+      <p>Squaree</p>
+    </div>
+    <div>
+      <UNavigationMenu arrow content-orientation="vertical" :items="items" class="w-full justify-center" />
+    </div>
+    <div>
+        <NuxtImg 
+        src="/Iconly-outline.svg" />
+    </div>
+  </header>
+</template>
---
+++ app/components/HeroBanner.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+const props = withDefaults(defineProps<{
+    titlePage?: string
+    description?: string
+}>(), {
+    titlePage: 'Predictable growth starts here',
+    description: 'Squaree is a platform that helps you grow your business by providing you with the tools you need to succeed.'
+})
+</script>
+
+<template>
+    <h1>{{ props.titlePage }}</h1>
+    <p>{{ props.description }}</p>
+</template>
